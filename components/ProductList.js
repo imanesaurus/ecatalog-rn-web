008e493b51fccfee85be8bdc2bad3342ee0ad35a
Gäste <pirmansah.otoluwa@gmail.com>
2020-09-17 16:07:22 +0700

    styling product list

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Image, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { PrimaryColor, AccentColor2, AccentColor } from "../constant/ColorsConst";
 
 const { width, height } = Dimensions.get("window");
 const ProductList = (props) => {
@@ -8,25 +16,61 @@ const ProductList = (props) => {
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
         marginHorizontal: 10,
         boxShadow: "0px 0px 5px rgba(0,0,0,.4)",
         marginVertical: 20,
         borderRadius: 10,
         maxWidth: 300,
-        maxHeight: 280,
-        overflow: 'hidden'
+        maxHeight: 350,
+        overflow: "hidden",
       }}
     >
-      <View style={{flex: 1, width: 300, height: 250, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
+      <View
+        style={{
+          flex: 1,
+          width: 300,
+          height: 250,
+          overflow: "hidden",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
         <Image
-          style={{width: 300, height: 250,}}
+          style={{ width: 300, height: 250 }}
           source={{ uri: props.image }}
         />
       </View>
-      <View style={{ justifyContent: "center", alignItems: "center", paddingVertical: 10, paddingHorizontal: 5, }}>
-        <Text style={styles.text}>{props.title}</Text>
-        <Text style={styles.text}>{props.price}</Text>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+        }}
+      >
+        <Text style={[styles.text, styles.title]}>{props.title}</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingHorizontal: 10,
+          paddingBottom: 10,
+        }}
+      >
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <TouchableOpacity>
+            <Text style={{ ...styles.text, fontWeight: "bold", fontSize: 20, color: AccentColor }}>
+              {props.price}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flex: 1 }}>
+          <TouchableOpacity>
+            <Text style={[styles.text, styles.touchable]}>Add To Cart</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -36,6 +80,22 @@ const styles = StyleSheet.create({
   text: {
     textAlign: "center",
   },
+  touchable: {
+    color: "white",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: AccentColor2,
+  },
+  title: {
+      fontSize: 18,
+    color: PrimaryColor,
+    textShadowRadius: 1,
+    textShadowOffset: {
+        width: 1,
+        height: 1,
+    },
+    textShadowColor: 'rgba(0,0,0,0.2)'
+  }
 });
 
 export default ProductList;
